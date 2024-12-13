e9716c474bc8428baf69114e3bc90337361a070e
ray03j <eihouse.potato@outlook.jp>
2024-12-13 10:19:40 +0900

    refactor: 移動したメソッドの削除

--- app/types/board.go
+++ app/types/board.go
@@ -1,8 +1,6 @@
 package types
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
-	"image/color"
 	"square-face-tetris/app/constants"
 )
 
@@ -16,20 +14,3 @@ func (b *Board) Init() {
 		(*b)[i] = make([]int, constants.BoardWidth)
 	}
 }
-
-
-// ボードの描画
-func (b *Board) Draw(screen *ebiten.Image) {
-	for y := 0 ; y < constants.BoardHeight/2 + constants.BoardHeight; y++ {
-		for x := 0 ; x < constants.BlockSize + constants.BoardWidth; x++ {
-			if (*b)[y][x] == 1 {
-				blockImage := ebiten.NewImage(constants.BlockSize, constants.BlockSize)
-				blockImage.Fill(color.RGBA{0, 0, 255, 255}) // 青
-				opts := &ebiten.DrawImageOptions{}
-				opts.GeoM.Translate(float64(x*constants.BlockSize), float64(y*constants.BlockSize))
-				screen.DrawImage(blockImage, opts)
-			}
-		}
-	}
-}
-
