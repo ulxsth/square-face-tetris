3429dc63df0e7c4955e77bb9af03b059ca6658ea
ulxsth <ulxsth@gmail.com>
2024-12-14 04:15:42 +0000

    chore: カメラのプレビューサイズをカメラ映像から参照するように修正

--- app/domain/wasm/camera.go
+++ app/domain/wasm/camera.go
@@ -28,6 +28,12 @@ var (
 	CanvasImage    *ebiten.Image
 	lastUpdateTime time.Time
 	updateInterval = time.Second / constants.CAMERA_PREVIEW_FPS
+
+	cameraWidth    int
+	cameraHeight   int
+	aspectRatio    float64
+	previewWidth   float64
+	previewHeight  float64
 )
 
 func InitCamera() {
@@ -43,8 +49,6 @@ func InitCamera() {
 	video = doc.Call(("createElement"), "video")
 	canvas = doc.Call(("createElement"), "canvas")
 	video.Set("muted", true)
-	video.Set("videoWidth", constants.ScreenWidth)
-	video.Set("videoHeight", constants.ScreenHeight)
 
 	// カメラの映像の取得権限をリクエスト
 	mediaDevices := js.Global().Get("navigator").Get("mediaDevices")
@@ -56,9 +60,20 @@ func InitCamera() {
 		stream = args[0]
 		video.Set("srcObject", stream)
 		video.Call("play")
-		canvas.Set("width", constants.ScreenWidth)
-		canvas.Set("height", constants.ScreenHeight)
-		ctx = canvas.Call("getContext", "2d")
+		video.Call("addEventListener", "loadedmetadata", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			cameraWidth = video.Get("videoWidth").Int()
+			cameraHeight = video.Get("videoHeight").Int()
+			canvas.Set("width", cameraWidth)
+			canvas.Set("height", cameraHeight)
+			ctx = canvas.Call("getContext", "2d")
+
+			// アスペクト比を計算
+			aspectRatio = float64(cameraHeight) / float64(cameraWidth)
+			previewWidth = float64(cameraWidth) * 0.25
+			previewHeight = previewWidth * aspectRatio
+
+			return nil
+		}))
 		return nil
 	}))
 }
@@ -75,11 +90,11 @@ func UpdateCamera() {
 		return
 	}
 
-	ctx.Call("drawImage", video, 0, 0, constants.ScreenWidth, constants.ScreenHeight)
+	ctx.Call("drawImage", video, 0, 0, cameraWidth, cameraHeight)
 	// canvas 経由で画面を base64 形式で取得
 	b64 := canvas.Call("toDataURL", "image/png").String()
 	ctx.Call("drawImage", video, 0, 0)
-	rgba := ctx.Call("getImageData", 0, 0, constants.ScreenWidth, constants.ScreenHeight, map[string]interface{}{
+	rgba := ctx.Call("getImageData", 0, 0, cameraWidth, cameraHeight, map[string]interface{}{
 		"willReadFrequently": true,
 	}).Get("data")
 	fmt.Println(rgba)
@@ -105,7 +120,7 @@ func DrawCameraPrev(screen *ebiten.Image) {
 
 	// 保持している ebiten.Image を右上に描画
 	opts := &ebiten.DrawImageOptions{}
-	opts.GeoM.Scale(0.25, 0.2) // サイズを固定
-	opts.GeoM.Translate(float64(constants.ScreenWidth-CanvasImage.Bounds().Dx()/4), 0)
+	opts.GeoM.Scale(previewWidth/float64(CanvasImage.Bounds().Dx()), previewHeight/float64(CanvasImage.Bounds().Dy()))
+	opts.GeoM.Translate(float64(constants.ScreenWidth)-previewWidth, 0)
 	screen.DrawImage(CanvasImage, opts)
 }
